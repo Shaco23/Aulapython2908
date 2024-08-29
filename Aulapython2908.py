f924d3e4b4b5b9a00d833385d082cc6d0f045540
Shaco23 <af553345@gmail.com>
2024-08-29 20:01:55 -0300

    Aula de exercicos operadores relacionais

---
+++ Aulapython2908.py
@@ -0,0 +1,34 @@
+dia = (input("Qual o dia de hoje?"))
+pedidoPizza = int(input("Quantas pizza comprou?"))
+pedidoBebida = int(input("Quantas Bebida comprou?"))
+pedidoBolo= int(input("Quantas Bolo comprou?"))
+pedidoDoce = int(input("Quantas Doce comprou?"))
+# Declara variaveis 
+
+diaFesta = 26
+
+pedidominimoPizza = 10
+pedidominimoBebida = 12
+pedidominimoBolo = 5
+pedidominimoDoce = 600
+
+if diaFesta == int:
+    print("Ana esta fazendo a compras no dia da festa")
+else:
+    print("Ana não esta fazendo no dia da festa")
+if(pedidominimoPizza <= pedidoPizza):
+    print("Ana não comprou pizza suficientes!")
+else:
+    print("Ana comprou pizza suficientes!")
+if(pedidominimoBebida <= pedidoBebida):
+    print("Ana comprou mais bebidas do que o necessario")
+else:
+    print("Ana comprou bebidas na quantidade correta")
+if(pedidominimoBolo <= pedidoBolo):
+    print("Ana exedeu a compra de bolo")
+else:
+    print("Ana comprou a quantidade correta de bolo")
+if(pedidominimoDoce > pedidoDoce):
+    print("Ana não comprou doces suficiente")
+else:
+    print("Ana comprou doces suficientes")
